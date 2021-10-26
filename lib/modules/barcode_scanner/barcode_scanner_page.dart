def44e2d5ad1466eaf7dfd5fb81214bb2806985a
oapmartins <octaviomartins10@gmail.com>
2021-10-25 23:19:01 -0300

    Corrigindo erro de travamento e bugs ao abrir a camera.

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -49,7 +49,7 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
             builder: (_, status, __) {
               if (status.showCamera) {
                 return Container(
-                  child: status.cameraController!.buildPreview(),
+                  child: controller.cameraController!.buildPreview(),
                 );
               } else {
                 return Container();
@@ -103,7 +103,7 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
                       'Tente escanear novamente ou digite o código do seu boleto.',
                   primaryLabel: 'Escanear novamente',
                   primaryOnPressed: () {
-                    controller.getAvailableCameras();
+                    controller.scanWithCamera();
                   },
                   secondaryLabel: 'Digitar código',
                   secondaryOnPressed: () {},
