61e8e94571ccb00b6fb4e3f4da735c867c20b725
oapmartins <octaviomartins10@gmail.com>
2021-11-11 23:16:27 -0300

    Começando a animar a tela de login

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -5,6 +5,7 @@ import 'package:payflow/shared/themes/app_images.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
+  //
 
   @override
   Widget build(BuildContext context) {
